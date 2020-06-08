7539c7c9b17f2334aec4a42524e98df7a1731caf
blitz.whtoo <whtoo@qq.com>
2020-06-08 11:38:55 +0800

    1. add grammar expr for function parser. 2. revise project Structure.

--- src/stone/FuncParser.java
+++ src/stone/FuncParser.java
@@ -4,17 +4,22 @@ import stone.ast.Arguments;
 import stone.ast.DefStmnt;
 import stone.ast.ParameterList;
 
+import java.nio.file.attribute.UserPrincipal;
+
 import static stone.Parser.rule;
 
 public class FuncParser extends BasicParser {
+    // param -> identifier
     Parser param = rule().identifier(reserved);
-    Parser params = rule(ParameterList.class)
-                        .ast(param).repeat(rule().sep(",").ast(param));
+    // params -> param { "," param }
+    Parser params = rule(ParameterList.class).ast(param).repeat(rule().sep(",").ast(param));
+    // paramList -> "(" params? ")"
     Parser paramList = rule().sep("(").maybe(params).sep(")");
-    Parser def = rule(DefStmnt.class)
-                     .sep("def").identifier(reserved).ast(paramList).ast(block);
-    Parser args = rule(Arguments.class)
-                      .ast(expr).repeat(rule().sep(",").ast(expr));
+    // def -> "def" indentifer paramList block
+    Parser def = rule(DefStmnt.class).sep("def").identifier(reserved).ast(paramList).ast(block);
+    // args -> expr { "," expr }
+    Parser args = rule(Arguments.class).ast(expr).repeat(rule().sep(",").ast(expr));
+    // postfix -> "(" args? ")"
     Parser postfix = rule().sep("(").maybe(args).sep(")");
 
     public FuncParser() {
